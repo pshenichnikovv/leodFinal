695c0e140db74c76635da5eacec531ad8686a46c
pshenichnikovv <52590081+pshenichnikovv@users.noreply.github.com>
2019-08-16 12:57:30 -0400

    Add files via upload

---
+++ assets/data/dishData.ts
@@ -0,0 +1,7 @@
+import { Dish } from '../../app/dish';
+
+export const MYDISHES: Dish[] = [
+    {id: '1111', make: 'Chinese', color: 'Blue'},
+    {id: '2222', make: 'Japanese', color: 'White'},
+    {id: '3333', make: 'American', color: 'Red'}
+];
